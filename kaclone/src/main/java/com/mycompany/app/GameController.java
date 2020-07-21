8200f9193ffcc4c7d82d0400194f01eff68c8062
Chris Reinschild <aerosecurity@gmail.com>
2020-07-21 19:32:46 -0400

    add userId cookie. generate new Id for new browser

--- kaclone/src/main/java/com/mycompany/app/GameController.java
+++ kaclone/src/main/java/com/mycompany/app/GameController.java
@@ -1,12 +1,33 @@
 package com.mycompany.app;
 
+import java.util.UUID;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GameController {
     @GetMapping("/game")
-    public String gamePage() {
+    public String gamePage(@CookieValue(value = "userId", defaultValue = "") String userId, 
+                            Model model, 
+                            HttpServletResponse response) {
+        UUID userUuid;
+        try {
+            userUuid = UUID.fromString(userId);
+        }
+        catch (IllegalArgumentException e) {
+            userUuid = UUID.randomUUID();
+            Cookie cookie = new Cookie("userId", userUuid.toString());
+            response.addCookie(cookie);
+        }
+
+        model.addAttribute("userId", userUuid.toString());
+
         return "game";
     }
 }
